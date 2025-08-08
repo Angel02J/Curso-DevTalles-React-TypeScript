b26062ebea75869c0174be7b02b7d9c666dc891d
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-08-08 08:34:23 -0600

    Variables y constantes

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,9 @@
+const firstName = 'Jose';
+const lastName = 'Lugo';
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const containsLetterL = lastName.includes('L');
+
+console.log({ containsLetterL, diceNumber, firstName, lastName });
