503b3af438c2a0e02b2e02669c57f1fbb14072c3
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-08-15 12:03:15 -0600

    Colocar estilos de CSS

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,9 +1,41 @@
+import type { CSSProperties } from "react";
+
+const firstName = 'Jose';
+const lastName = 'Lugo';
+
+const favoriteGames = ['Elden Ring', 'Smash', 'Metal Gears'];
+const isActive = true;
+
+const address = {
+    zipCode: 'ABC-123',
+    country: 'Canada'
+}
+
+const myStyles: CSSProperties = {
+    backgroundColor: '#fafafa',
+    borderRadius: 20,
+    padding: 10,
+    marginTop: 30,
+}
 
 const MyAwesomeApp = () => {
+
+
+
     return (
         <>
-            <h1>Jose</h1>
-            <h3>Lugo</h3>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3>
+
+            <p>{favoriteGames.join(', ')}</p>
+
+            <h1>{isActive ? 'Activo' : 'No activo'}</h1>
+
+            <p
+                style={myStyles}
+            >
+                {JSON.stringify(address)}
+            </p>
         </>
     )
 }
