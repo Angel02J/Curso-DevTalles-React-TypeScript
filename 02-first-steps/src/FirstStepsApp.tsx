38c594dace032811ad3c9f09836742b8b87b4616
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-08-15 10:22:56 -0600

    Mi primeri componente

---
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -0,0 +1,13 @@
+export function FirtsStepsApp() {
+    return (
+        <>
+            <h1>Hola mundo</h1>
+            <p>Esto es un párrafo</p>
+            <button>Click me</button>
+
+            <div>
+                <h2>Hola dentro de un div</h2>
+            </div>
+        </>
+    )
+}
--- 02-first-steps/src/main.tsx
+++ 02-first-steps/src/main.tsx
@@ -1,8 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { FirtsStepsApp } from './FirstStepsApp';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hola mundo</h1>
+    <FirtsStepsApp/>
   </StrictMode>,
 );
