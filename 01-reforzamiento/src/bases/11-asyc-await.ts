bb9bb112fa48f66ce96f533e1357f6df4c0e1fe9
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-08-15 09:48:12 -0600

    Async - await

---
+++ 01-reforzamiento/src/bases/11-asyc-await.ts
@@ -0,0 +1,26 @@
+import type { GiphyRandomResponse } from "../data/giphy.response";
+
+const API_KEY = 'J8La7AJBucGpm7TL6nacPwfZCsfQX45g';
+
+
+
+const createImageInsideDOM = (url: string) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
+
+    document.body.append(imgElement);
+}
+
+const getRandomGifUrl = async() => {
+
+    const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
+
+    const { data }: GiphyRandomResponse = await response.json();
+    
+
+    return data.images.original.url;
+};
+
+getRandomGifUrl().then(
+    url => createImageInsideDOM(url)
+);
