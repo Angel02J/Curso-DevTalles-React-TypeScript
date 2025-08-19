ae2de01b3f05e1b054d69ee83d5811872b1f2745
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-08-19 08:10:19 -0600

    Hook - useState

--- 02-first-steps/src/ItemCounter.tsx
+++ 02-first-steps/src/ItemCounter.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 
 interface Props {
     name: string;
     quantity?: number;
 }
 
-const ItemCounter = ({ name, quantity }: Props) => {
+const ItemCounter = ({ name, quantity = 1 }: Props) => {
+    const [count, setCount] = useState(quantity);
 
-    const handleClick = () => {
-        console.log(`Click en ${name}`)
+    const handleAdd = () => {
+        setCount(count + 1);
+    };
+
+    const handleSubtract = () => {
+        if (count === 1) return;
+
+        setCount(count - 1);
     }
 
     return (
@@ -23,10 +31,12 @@ const ItemCounter = ({ name, quantity }: Props) => {
                 }}
             >{name}</span>
             <button
-                onClick={handleClick}
+                onClick={handleAdd}
             >+1</button>
-            <span>{quantity}</span>
-            <button>-1</button>
+            <span>{count}</span>
+            <button
+                onClick={handleSubtract}
+            >-1</button>
         </section>
     )
 }
