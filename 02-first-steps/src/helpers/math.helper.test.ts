9e33008166d5fdf097aea78d8b189dd24c8c0c9f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-08-19 11:57:01 -0600

    Mis primeras pruebas automaticas

--- 02-first-steps/src/helpers/math.helper.test.ts
+++ 02-first-steps/src/helpers/math.helper.test.ts
@@ -2,6 +2,11 @@ import { expect, test } from 'vitest';
 import { add } from './math.helper';
 
 test('should add two numbers', () => {
+
+    //! 1. Arrange
+    //! 2. Act
+    //! 3. Assert
+
     const result = add(1, 3);
     expect(result).toBe(4);
 });
