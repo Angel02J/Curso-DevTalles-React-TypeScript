32fb6c3544b305cf8611396d8baa082abb31c85f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-08-20 10:42:46 -0600

    Evaluar snapshots

--- 02-first-steps/src/MyAwesome.test.tsx
+++ 02-first-steps/src/MyAwesome.test.tsx
@@ -34,4 +34,16 @@ describe('MyAwesomeApp', () => {
         const h1 = screen.getByTestId('first-name-title');
         expect(h1.innerHTML).toContain('Jose');
     });
+
+    test('Should match snapshot', () => {
+        const { container } = render(<MyAwesomeApp />);
+
+        expect(container).toMatchSnapshot();
+    });
+
+    test('Should match snapshot', () => {
+        render(<MyAwesomeApp />);
+
+        expect(screen.getByTestId('div-app')).toMatchSnapshot();
+    });
 });
